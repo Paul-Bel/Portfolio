b0ad2856886438c346a44696d80d7d1899501f7d
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-01-09 21:59:43 +0300

    add style__

--- src/Skills/Skill.tsx
+++ src/Skills/Skill.tsx
@@ -11,11 +11,14 @@ export const Skill = (props: SkillTypeProps) => {
 
     return (
         <div className={s.skill}>
+
             <div className={s.icon}>pages</div>
+
             <div className={s.about}>
                 <h3>{props.skill}</h3>
                 <span className={s.aboutSkill}>{props.about}</span>
             </div>
+
         </div>
     )
 }
--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -4,20 +4,31 @@ import {Skill} from "./Skill";
 import stylesContainer from "../common/styles/Container.module.css";
 
 export const Skills = () => {
-const [text, setText] = useState([
-    {id: 1, skill: "HTML", aboute: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, quis."},
-    {id: 2, skill: "CSS", aboute: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, quis."},
-    {id: 3, skill: "React", aboute: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At beatae consectetur cupiditate deserunt earum, expedita fuga iste magnam nam, necessitatibus nemo neque nisi, optio provident sed tempore vel veritatis vitae?"},
-])
+    const [text, setText] = useState([
+        {id: 1, skill: "HTML", aboute: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, quis."},
+        {id: 2, skill: "CSS", aboute: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, quis."},
+        {
+            id: 3,
+            skill: "React",
+            aboute: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At beatae consectetur cupiditate deserunt earum, expedita fuga iste magnam nam, necessitatibus nemo neque nisi, optio provident sed tempore vel veritatis vitae?"
+        },
+    ])
     let skill = text.map(el => {
         return <Skill skill={el.skill} id={el.id} about={el.aboute}/>
     })
     return (
         <div className={s.skillBlock}>
             <div className={`${stylesContainer.containerMain} ${s.container}`}>
-                <h2 className={s.header}>My skills</h2>
+                <div className={s.skillItem}>
+                <h2 className={s.header}>
+                    My skills
+                </h2>
+                </div>
                 <div className={s.skills}>
                     {skill}
+                    {/*<Skill id={44} skill={'ffffref'} about={'refergregergergergegeg'}/>*/}
+                    {/*<Skill id={44} skill={'ffffref'} about={'refergregergergergegeg'}/>*/}
+                    {/*<Skill id={44} skill={'ffffref'} about={'refergregergergergegeg'}/>*/}
                 </div>
             </div>
         </div>
