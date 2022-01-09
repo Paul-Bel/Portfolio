e6b90c2c4446b121011dedaf9ba29f9b90214114
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-01-10 01:52:45 +0300

    seven stagy_finish

--- src/common/Footer/Footer.tsx
+++ src/common/Footer/Footer.tsx
@@ -1,44 +1,30 @@
 import React from "react";
 import s from './Footer.module.css'
 
-const squares = (a: number) => {
-    let i = 0
-    while ( i < a) {
-        if (i % 2 === 0) {
-            console.log('if')
-            return <div className={s.squares}></div>
-        } i++
-        if (i % 2 === 1) {
-            console.log('else')
-            return <div className={s.square}></div>
-        }
-        i++
-    }
-}
 
 export const Footer = () => {
-
-
-
-
-return (
-    <div className={s.footer}>
-        <div>
-            <h3>Pavel Beloborodov</h3>
-            <div className={s.containerSqu}>
-                {/*{squares(7)}*/}
-
-            <div className={s.squares}></div>
-            <div className={s.square}></div>
-            <div className={s.squares}></div>
-            <div className={s.square}></div>
-            <div className={s.squares}></div>
-            <div className={s.square}></div>
-            <div className={s.squares}></div>
-            <div className={s.square}></div>
+    const squares = (a: number) => {
+        let i = 0
+        let arr: Array<any> = []
+        while (i < a) {
+            if (i % 2 === 0) {
+                arr = [...arr, <div className={s.squares}></div>]}
+            i++
+            if (i % 2 === 1) {
+                arr = [...arr, <div className={s.square}></div>]}
+            i++
+        }
+        return arr
+    }
+    return (
+        <div className={s.footer}>
+            <div>
+                <h3>Pavel Beloborodov</h3>
+                <div className={s.containerSqu}>
+                    {squares(7)}
+                </div>
+                <span>© 2022 Все права защищены</span>
             </div>
-            <span>© 2022 Все права защищены</span>
         </div>
-    </div>
-)
+    )
 }
