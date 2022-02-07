f8766089b66e974e884f0339969f393113812907
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-02-08 00:57:12 +0300

    add mixin

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import s from './Contacts.module.css'
+import s from './Contacts.module.scss'
 
 export const Contacts = () => {
 
     return (
         <div className={s.block}>
-            <div
-                // className={`${stylesContainer.containerMain} ${s.blockContacts}`
-                className={s.blockContact}
-                >
-                <div className={s.title}><h3>Contacts</h3></div>
+            <div className={s.blockContact}>
+                <div><h3>Contacts</h3></div>
                 <form className={s.input}>
                     <input type="text"/>
                     <input type="text"/>
