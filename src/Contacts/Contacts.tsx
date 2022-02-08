f7d8e4a875e9cb24347f70dac5a3b48c49e4bf7b
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-02-08 12:47:36 +0300

    added footer and hire me

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import s from './Contacts.module.scss'
+import {Title} from "../common/components/Title/Title";
 
 export const Contacts = () => {
 
     return (
         <div className={s.block}>
             <div className={s.blockContact}>
-                <div><h3>Contacts</h3></div>
+                <div><Title title={"Contacts"}/></div>
                 <form className={s.input}>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <textarea> </textarea>
+                    <input type="text" placeholder={"Name"}/>
+                    <input type="text" placeholder={"E-mail"}/>
+                    <textarea rows={3} placeholder={"Your message"}> </textarea>
                     <button type={"submit"}>send</button>
                 </form>
 
--- src/common/Work_remotely/WorkRemotely.tsx
+++ src/common/Work_remotely/WorkRemotely.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import s from './Work_remotely.module.css'
+import s from './Work_remotely.module.scss'
+import {Title} from "../components/Title/Title";
 
 export const WorkRemotely = () => {
 
@@ -8,7 +9,7 @@ export const WorkRemotely = () => {
         <div className={s.block}>
             <div className={s.container}>
                 <div className={s.title}>
-                    considering options for remote work
+                    <Title title={"considering options for remote work"}/>
                 </div>
                 <div className={s.click}>
                     <button>hire me</button>
