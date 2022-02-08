693d9741b538ffdc288b40a2a74c788c1115496b
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-02-08 23:20:59 +0300

    added white input

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -7,7 +7,7 @@ export const NavBar = () => {
     return (
         <div className={s.nav}>
             <ul className={s.listMenu}>
-                <li className={s.menuItem} ><a href="#1">Home</a></li>
+                <li className={s.menuItem} ><a href="#1">Menu</a></li>
                 <li className={s.menuItem}><a href="#2">Skills</a></li>
                 <li className={s.menuItem}><a href="#3">Projects</a></li>
                 <li className={s.menuItem}><a href="#4">Contacts</a></li>
