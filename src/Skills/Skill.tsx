c041f90c4ec772ef65258ee9311f157fd38070b9
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-02-17 11:12:22 +0300

    added animation photo

--- src/Skills/Skill.tsx
+++ src/Skills/Skill.tsx
@@ -6,15 +6,17 @@ type SkillTypeProps = {
     id: number,
     skill: string,
     about: string,
+    style: {backgroundImage: string}
 }
 export const Skill = (props: SkillTypeProps) => {
-
     return (
         <div className={s.skill}>
-            <div className={s.icon}>pages</div>
-            <div className={s.about}>
+            <div className={s.icon} style={props.style}>pages</div>
+            <div className={s.about} >
                 <h3>{props.skill}</h3>
-                <span className={s.aboutSkill}>{props.about}</span>
+
+                {/*<span className={s.aboutSkill}>{props.about}</span>*/}
+
             </div>
 
         </div>
--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -3,19 +3,21 @@ import s from './Skills.module.scss'
 import {Skill} from "./Skill";
 import stylesContainer from "../common/styles/Container.module.scss";
 import {Title} from "../common/components/Title/Title";
+import react from "../assets/img/R.jpg";
 
 export const Skills = () => {
     const text = [
-        {id: 1, skill: "HTML", about: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, quis."},
-        {id: 2, skill: "CSS", about: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, quis."},
+        {id: 1, skill: "React", about: "Lorem ipsum quis.", style: {backgroundImage: `url(${react})`},},
+        {id: 2, skill: "CSS", about: "Lorem quis.", style: {backgroundImage: `url(${''})`},},
         {
             id: 3,
-            skill: "React",
-            about: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At beatae consectetur cupiditate deserunt earum, expedita fuga iste magnam nam, necessitatibus nemo neque nisi, optio provident sed tempore vel veritatis vitae?"
+            skill: "HTML",
+            about: "vel veritatis vitae?",
+            style: {backgroundImage: `url(${''})`},
         },
     ]
     let skill = text.map(el => {
-        return <Skill skill={el.skill} id={el.id} about={el.about}/>
+        return <Skill style={el.style} skill={el.skill} id={el.id} about={el.about}/>
     })
     return (
         <div className={s.skillBlock} id={'2'} >
