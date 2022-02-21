2cac06d6ac12114f4d4fd30c8cd3e059842e297c
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-02-22 01:11:01 +0300

    added links contact

--- src/MyWorks/MyWork.tsx
+++ src/MyWorks/MyWork.tsx
@@ -2,24 +2,23 @@ import s from './My_works.module.scss'
 
 
 
-type WorkPropsType = { title: string, about: string, styleImg: {backgroundImage: string}, project: string, img: string }
+type WorkPropsType = {
+    title: string, about: string,
+    styleImg: {backgroundImage: string},
+    project: string,
+    img: string }
 
 export const MyWork = (props: WorkPropsType) => {
     console.log(props.project)
     return (
         <div className={s.work}>
-            <div className={s.img}
-                 // onClick={href={}}
-                 // style={props.styleImg}
-            >
+            <div className={s.img}>
                 <img src={props.img}
                      className={s.project}
                      alt=""/>
                 <a href={props.project}
                    target={'_blank'}
-                   // className={s.button}
-
-                >looks</a>
+                      >looks</a>
             </div>
             <div className={s.about}>
                 <h3>{props.title}</h3>
--- src/common/Footer/Footer.tsx
+++ src/common/Footer/Footer.tsx
@@ -1,27 +1,32 @@
 import React from "react";
 import s from './Footer.module.scss'
+import IN from '../../assets/img/forLinks/linkedIn.png'
+import HH from '../../assets/img/forLinks/HeadHunter_logo.png'
+import Tel from '../../assets/img/forLinks/telegram.svg'
+import GH from '../../assets/img/forLinks/GH.png'
+
+const links = [
+    {id: 1, profilePage: 'Linkedin', link: 'https://www.linkedin.com/in/paul-beloborodov-b9812921a/', style: `${IN}` },
+    {id: 2, profilePage: 'HH', link: 'https://voronezh.hh.ru/resume/a681457aff0987bd6a0039ed1f7a5566476c4c', style: `${HH}` },
+    {id: 3, profilePage: 'Telegram', link: 'https://t.me/Bel_Paul', style:  `${Tel}` },
+    {id: 4, profilePage: 'GitHub', link: 'https://github.com/Paul-Bel', style:  `${GH}` },
+    // {id: 5, profilePage: 'CV', link: '', style: 'string' },
+]
 
 
 export const Footer = () => {
-    const squares = (a: number) => {
-        let i = 0
-        let arr: Array<any> = []
-        while (i < a) {
-            if (i % 2 === 0) {
-                arr = [...arr, <div className={s.squares}> </div>]}
-            i++
-            if (i % 2 === 1) {
-                arr = [...arr, <div className={s.square}> </div>]}
-            i++
-        }
-        return arr
-    }
+
+    const linksContact = links.map((m) => {
+        return  <div key={m.id+m.profilePage} className={s.contactMe}> <img className={s.square} src={m.style} alt={m.profilePage}/>
+            <a className={s.squares} href={m.link} target={'_blank'}>   </a></div>
+    })
+
     return (
         <div className={s.footer}>
             <div>
                 <h3>Paul Beloborodov</h3>
                 <div className={s.containerSqu}>
-                    {squares(7)}
+                    {linksContact}
                 </div>
                 <span>© 2022 Все права защищены</span>
             </div>
